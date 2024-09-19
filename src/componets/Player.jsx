3faac5f1d79d190ff96a6f60de300b7d0155da7f
wjdwlduq1 <wjdwwuq1@gmail.com>
2024-09-19 17:54:54 +0900

    양방향 바인딩

--- src/componets/Player.jsx
+++ src/componets/Player.jsx
@@ -31,10 +31,21 @@ function Player({ initialName, symbol }) {
     setIsEditing((editing) => !editing);
   }
 
+  function handleChange(evt) {
+    setPlayerName(evt.target.value);
+  }
+
   let editablePlayerName = <span className='player-name'>{playerName}</span>;
   !isEditing
     ? editablePlayerName
-    : (editablePlayerName = <input type='text' value={playerName} required />);
+    : (editablePlayerName = (
+        <input
+          type='text'
+          value={playerName}
+          onChange={handleChange}
+          required
+        />
+      ));
 
   return (
     <li>
