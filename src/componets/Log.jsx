ada2061b4354ae188e8d2fa5883115e221014d62
wjdwlduq1 <wjdwwuq1@gmail.com>
2024-09-24 14:12:23 +0900

    js 백텍

--- src/componets/Log.jsx
+++ src/componets/Log.jsx
@@ -1,9 +1,10 @@
 function Log({ turns }) {
+  console.log('turns : ', turns);
   return (
     <ol id='log'>
       {turns.map((turn) => (
-        /* js 문법  */
-        <li key={'${turn.square.row}${turns.square.col}'}>
+        /* js 문법  백틱 숫자 1 옆에 있는 특수 기호*/
+        <li key={`${turn.square.row}${turn.square.col}`}>
           {turn.player} player selected col : {turn.square.row}, row :{' '}
           {turn.square.col}{' '}
         </li>
